ba7ebdb652cd38f7331a39d69b6f09040c313515
Kenia Crisoni <keniafcrisoni@gmail.com>
2023-09-06 22:24:41 -0300

    exercicio 1

---
+++ src/Exercicios1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicios1 {
+    public static void main(String[] args) throws Exception {
+       Scanner sc = new Scanner(System.in);
+
+       int A, B, soma;
+
+       A = 10 ;
+       B = 30 ;
+
+       soma = A+B;
+
+       
+        System.out.println("SOMA= " + soma);
+
+        sc.close();
+    }
+}
